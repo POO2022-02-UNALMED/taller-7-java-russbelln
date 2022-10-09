dc363e37e9d67522925431fbcecc078eb5fcea13
russbelln <92810561+russbelln@users.noreply.github.com>
2022-10-09 18:39:56 -0500

    Update Alfabeto.java

--- src/main/java/comunicacion/Alfabeto.java
+++ src/main/java/comunicacion/Alfabeto.java
@@ -20,11 +20,8 @@ public class Alfabeto extends Pictograma{
 
     @Override
     public String toString() {
-        String texto="";
-        for (String i:
-             this.letras) {
-            texto= texto+", "+i;
-        }
+        String texto="A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z";
+       
         return texto;
     }
 }
